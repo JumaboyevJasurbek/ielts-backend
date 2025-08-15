c1947d48772ee74699c730709a98a53e2cb96a41
JumaboyevJasurbek <jumaboyevjasurbek2004@gmail.com>
2025-08-15 09:11:24 +0500

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,PATCH,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+
+  // CORS setup
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
